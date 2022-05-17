85c73d037c3045f272ff0873849da774c85b2911
BaptisteM44 <bapmorvan@gmail.com>
2022-05-17 15:44:33 +0200

    modif horizontal section

--- script.js
+++ script.js
@@ -1,50 +1,16 @@
-// scroll horizontal
-/**
-* By Alvaro Trigo 
-* Follow me on Twitter: https://twitter.com/imac2
-*/
-(function(){
-    init();
+gsap.registerPlugin(ScrollTrigger);
 
-    var g_containerInViewport;
-    function init(){
-        setStickyContainersSize();
-        bindEvents();
-    }
+let sections = gsap.utils.toArray(".panel");
 
-    function bindEvents(){
-        window.addEventListener("wheel", wheelHandler);        
-    }
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".location",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () => "+=" + document.querySelector(".location").offsetWidth
+  }
+});
 
-    function setStickyContainersSize(){
-        document.querySelectorAll('.sticky-container').forEach(function(container){
-            const stickyContainerHeight = container.querySelector('main').scrollWidth;
-            container.setAttribute('style', 'height: ' + stickyContainerHeight + 'px');
-        });
-    }
-
-    function isElementInViewport (el) {
-        const rect = el.getBoundingClientRect();
-        return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
-    }
-
-    function wheelHandler(evt){
-        
-        const containerInViewPort = Array.from(document.querySelectorAll('.sticky-container')).filter(function(container){
-            return isElementInViewport(container);
-        })[0];
-
-        if(!containerInViewPort){
-            return;
-        }
-
-        var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
-        var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
-        let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
-
-        if(g_canScrollHorizontally){
-            containerInViewPort.querySelector('main').scrollLeft += evt.deltaY;
-        }
-    }
-})();
-// END scroll horizontal
