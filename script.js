ebcdc12a06c5caf32eecec3203d67be7709b124d
BaptisteM44 <bapmorvan@gmail.com>
2022-09-29 12:15:52 +0200

    change middle+photo

--- script.js
+++ script.js
@@ -13,31 +13,21 @@ gsap.to(sections, {
     end: () => "+=" + document.querySelector(".location").offsetWidth,
   },
 });
-// gsap.to(".about_content_img", "(min-width: 733px)", {
-//   yPercent: 20,
-//   duration: 1,
-//   scrollTrigger: {
-//     trigger: ".about",
-//     scrub: true,
-//     start: "center center",
-//   },
-// });
-gsap.to(".about_content_right", {
-  "(max-width: 733px)": {
-    yPercent: 11,
-    duration: 1,
-    scrollTrigger: {
-      trigger: ".about",
-      scrub: true,
-      start: "center center",
-    },
+gsap.to(".about_content_img", {
+  yPercent: 30,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".about",
+    scrub: true,
+    start: "top center",
   },
 });
-// ScrollTrigger.matchMedia({
-//   "(max-width: 2600px)": function() {
-//       gsap.set(".link", {
-//           y: 1670,
-//           xPercent: -15,
-//           skewY: 30,
-//           scale: 1.6,
-//       });
+gsap.to(".about_content_right", {
+  yPercent: 30,
+  duration: 1,
+  scrollTrigger: {
+    trigger: ".about",
+    scrub: true,
+    start: "top center",
+  },
+});
