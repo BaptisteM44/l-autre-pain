35f577f96f482ebf7a1f296a94a7aa41fe046807
BaptisteM44 <bapmorvan@gmail.com>
2022-09-29 19:03:51 +0200

    modif

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".panel");
 
@@ -13,21 +13,42 @@ gsap.to(sections, {
     end: () => "+=" + document.querySelector(".location").offsetWidth,
   },
 });
-gsap.to(".about_content_img", {
-  yPercent: 30,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".about",
-    scrub: true,
-    start: "top center",
-  },
-});
-gsap.to(".about_content_right", {
-  yPercent: 40,
-  duration: 1,
-  scrollTrigger: {
-    trigger: ".about",
-    scrub: true,
-    start: "top center",
-  },
-});
+// gsap.to(".about_content_img", {
+//   yPercent: 30,
+//   duration: 1,
+//   scrollTrigger: {
+//     trigger: ".about",
+//     scrub: true,
+//     start: "top center",
+//   },
+// });
+// gsap.to(".about_content_right", {
+//   yPercent: 40,
+//   duration: 1,
+//   scrollTrigger: {
+//     trigger: ".about",
+//     scrub: true,
+//     start: "top center",
+//   },
+// });
+const tl = gsap.timeline();
+
+tl.from(".title1, img", 2, {
+  y: 80,
+  ease: "power4.out",
+  delay: 0.6,
+  skewY: 4,
+  opacity:0,
+  stagger: {
+    amount: 0.3
+  }
+})
+.from(".nav_ul", 2, {
+  x: 80,
+  ease: "power4.out",
+  delay: -1.8,
+  opacity:0,
+  stagger: {
+    amount: 0
+  }
+})
